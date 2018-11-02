c249616d353ecff6e44cc95925c6ceadddc2de7d
Matt Williams <matt@williams-tech.net>
2018-11-02 17:19:16 -0400

    Moving to yarn because NPM is killing me

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,5 +1,3 @@
 @echo off
 
-export NPM_CONFIG_LOGLEVEL=debug
-export NODE_VERBOSE=true
-npm install && node_modules\.bin\hubot.cmd --name "triatomic" %* 
+yarn install && node_modules\.bin\hubot.cmd --name "triatomic" %* 
